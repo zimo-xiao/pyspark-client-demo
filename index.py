9b5a0e1f886d977b50449c99153ba5bf7c23cb73
Zimo Xiao <xiaozimo@zuggr.com>
2020-05-26 16:14:22 -0500

    init

---
+++ index.py
@@ -0,0 +1,22 @@
+import pyspark
+import os
+
+os.environ['YARN_CONF_DIR'] = os.getcwd() + '/conf'
+os.environ["HADOOP_USER_NAME"] = 'root'
+
+conf = pyspark.SparkConf()
+conf.setAppName('hello')
+conf.setMaster('yarn')
+conf.set('spark.submit.deployMode', 'client')
+
+sc = pyspark.SparkContext(conf=conf)
+
+data = [1, 2, 3, 4, 5, 8, 9]
+
+myRDD = sc.parallelize(data)
+
+myRDD.collect()
+
+myRDD.count()
+
+print(myRDD)
